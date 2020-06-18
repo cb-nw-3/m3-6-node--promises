698279815025d0b77ab3717dce89824e5dc8386d
jenny ma <jenny_lau_8@hotmail.com>
2020-06-18 19:13:25 -0400

    E1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,10 +1,28 @@
 // Exercise 1 - `getIssPosition`
 // -----------------------------
 
-// require the 'request-promise' module.
-
+// require the 'request-promise' module from package.json
+const request = require('request-promise');
 
 // Returns the current position of the ISS
 function getIssPosition() {
+    //fetch the information from the external source http link
+    return request('http://api.open-notify.org/iss-now.json')
 
-}
+        .then(response => {
+            //parse as JSON
+            const issLocation = JSON.parse(response);
+            return {
+                lat: issLocation.iss_position.latitude,
+                lng: issLocation.iss_position.longitude,
+            }
+        })
+        //data is the previous object with values lat and lng
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+        .catch(error => console.log('Error: ', error));
+}       
+
+getIssPosition();
