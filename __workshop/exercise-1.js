31064220e99c4a77794b7be584718beb9c713ba1
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-06-18 16:05:16 -0400

    exercise 1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -2,9 +2,27 @@
 // -----------------------------
 
 // require the 'request-promise' module.
-
+const rp = require('request-promise');
 
 // Returns the current position of the ISS
 function getIssPosition() {
-
+  rp('http://api.open-notify.org/iss-now.json')
+    // parse the JSON to make it a javascript object
+    .then(JSON.parse)
+    // store the data in an object
+    .then((data) => {
+      // console.log(`ISS Latitude: ${data.iss_position.latitude} \nISS Longitude: ${data.iss_position.longitude}`)
+      return {
+        lat: data.iss_position.latitude,
+        lng: data.iss_position.longitude
+      }
+    })
+    // log the object that the previous promise returned
+    .then((result) => console.log(result))
+    // error catcher if there's a problem with the data
+    .catch((error) => {
+      console.log('Cannot find the position for the ISS')
+    })
 }
+
+getIssPosition();
