980d7e24817120f2d79db111481644d128639915
Andres Rueda <garo99@gmail.com>
2020-06-19 12:58:51 -0400

    exercise 1 using fetch

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -3,8 +3,40 @@
 
 // require the 'request-promise' module.
 
+//https://wheretheiss.at/w/developer
 
 // Returns the current position of the ISS
-function getIssPosition() {
+const fetch = require('node-fetch');
 
+function getIssPosition() {
+  url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) =>
+      console.log('Latitude: ', data.latitude, 'Longitude:', data.longitude)
+    );
 }
+
+getIssPosition();
+
+//This one works on the browser
+//function getIssPosition() {
+//   url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+//   fetch(url).then((response) => {
+//     const reader = response.body.getReader();
+//     const decoder = new TextDecoder();
+
+//     reader
+//       .read()
+//       .then(function process(result) {
+//         if (result.done) return;
+//         const info = JSON.parse(decoder.decode(result.value, { stream: true }));
+//         console.log('lat', info.latitude, 'lng', info.longitude);
+//       })
+//       .then(() => {
+//         console.log('All done!');
+//       });
+//   });
+// }
