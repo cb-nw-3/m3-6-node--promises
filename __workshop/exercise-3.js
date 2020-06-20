ae04c4b17fa8131205287ab116cd031bd05f3005
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-20 11:05:38 -0400

    removing new.js file with app key

--- __workshop/exercise-3.js
+++ __workshop/exercise-3.js
@@ -1,7 +1,6 @@
 // Exercise 3 - `getAddressPosition`
 // ---------------------------------
+require('dotenv').config();
 
 // Given a position (latitude and longitude), returns the position
-function getCurrentTemperatureAtPosition(position) {
-
-}
+function getCurrentTemperatureAtPosition(position) {}
