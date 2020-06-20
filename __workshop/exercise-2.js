9ec7652cabe6eb46b4da0f26c05b10a5a130d85f
donnyesq <adonis@darmas.ca>
2020-06-20 11:32:08 -0400

    exercises 1 and 2 complete

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -1,10 +1,36 @@
 // Exercise 2 - `getAddressPosition`
 // ---------------------------------
+const dotenv = require("dotenv");
+dotenv.config();
+
+const opencage = require("opencage-api-client");
 
 function getAddressPosition(address) {
-    const requestObj = {
-        key: '<MY_API_KEY>',
-        q: address
-    };
+  const requestObj = {
+    key: "<API_KEY>",
+    q: address,
+  };
 
+  return opencage
+    .geocode(requestObj)
+    .then((data) => {
+      if (data.status.code == 200) {
+        if (data.results.length > 0) {
+          const place = data.results[0];
+          return {
+            lat: place.geometry.lat,
+            lng: place.geometry.lng,
+          };
+        }
+      } else {
+        console.log("error", data.status.message);
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => console.log("Error:", error));
 }
+
+getAddressPosition("HaYarkon St 205, Tel Aviv-Yafo, Israel");
