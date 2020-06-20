eedbcfc4b5f23cd53e28d653943f06120caa50fe
rogerlam <rogerlam@wylco.org>
2020-06-20 10:33:10 -0400

    ex 1 complete

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -3,8 +3,19 @@
 
 // require the 'request-promise' module.
 
+const rp = require("request-promise");
 
 // Returns the current position of the ISS
 function getIssPosition() {
+    rp('http://api.open-notify.org/iss-now.json')
+        .then(response => {
+            let locationISS = JSON.parse(response);
+            return {
+                lat: locationISS.iss_position.latitude,
+                lng: locationISS.iss_position.longitude
+            }
+        })
+        .then(data => console.log(data));
+};
 
-}
+getIssPosition();
