fff764c099bb5c0c8c1c808955b1491e25ffe093
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-06-20 13:18:36 -0400

    readd soft reset changes

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -3,8 +3,51 @@
 
 // require the 'request-promise' module.
 
+//yarn add request-promise
+
+const rp = require("request-promise");
 
 // Returns the current position of the ISS
-function getIssPosition() {
 
+// const options = {
+//   method: "POST",
+//   uri: "http://api.open-notify.org/iss-now.json",
+//   body: {
+//     some: "payload",
+//   },
+//   json: true,
+// };
+
+function getIssPosition() {
+  rp("http://api.open-notify.org/iss-now.json")
+    .then(function (data) {
+      //set the data stream into an object instead of a string
+      //console.log(typeOf(data));
+      const obj = JSON.parse(data);
+      //console.log(obj);
+
+      //subset the obj to the iss_position array
+      const pos = obj.iss_position;
+
+      //obtain the latitude and longitude from the array
+      let lng = pos.longitude;
+      let lat = pos.latitude;
+
+      //print out the position;
+      return console.log(
+        `ISS is currently at latitude: ${lat} and longitude: ${lng}`
+      );
+    })
+    .catch(function (error) {
+      //console.log(error.statusCode);
+      const errCode = error.statusCode;
+
+      //print out a simple error message
+      console.log(`An error has occured with status code: ${errCode}`);
+
+      //a simple test would be to remove a letter from the rp(url)
+    });
 }
+
+//call the function
+getIssPosition();
--- __workshop/exercise-3.js
+++ __workshop/exercise-3.js
@@ -2,6 +2,40 @@
 // ---------------------------------
 
 // Given a position (latitude and longitude), returns the position
-function getCurrentTemperatureAtPosition(position) {
 
+//only works where .env file is located in workshop folder
+const rp = require("request-promise");
+require("dotenv").config();
+
+//DARSKY API REQUEST
+//https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+
+function getCurrentTemperatureAtPosition(position) {
+  const requestObj = {
+    key: process.env.DARKSKY_API_KEY,
+    latitude: position.lat,
+    longitude: position.lng,
+  };
+
+  let requestAPI = `https://api.darksky.net/forecast/${requestObj.key}/${requestObj.latitude},${requestObj.longitude}`;
+  //console.log(requestAPI);
+  return rp(requestAPI)
+    .then(JSON.parse)
+    .then((result) => {
+      console.log(result.currently.temperature);
+      return result.currently.temperature;
+    })
+    .catch((error) => {
+      console.log(error.statusCode);
+    });
 }
+
+//EXERCISE 2 OUTPUTS THIS
+//which we will use to request the temperature
+let testPos = { lat: 45.497118, lng: -73.579044 };
+
+getCurrentTemperatureAtPosition(testPos);
+
+module.exports = {
+  getCurrentTemperatureAtPosition,
+};
