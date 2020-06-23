53ca71c0a79cf0d67942b72b35525abcb80be1eb
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-22 22:34:32 -0400

    stretc goal

---
+++ __workshop/guess-the-number.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+const hiddenNumber = Math.floor(Math.random() * 100) + 1;
+console.log('This is the secret target:', hiddenNumber);
+
+const question = [
+  {
+    type: 'number',
+    name: 'answer',
+    message: 'Guess the hidden number between 1 and 100\n',
+  },
+];
+
+let counter = 5;
+inquirer.prompt(question).then(recursive);
+function recursive(answer) {
+  if (counter <= 1) {
+    console.log('=( you lost');
+    return;
+  }
+  answer.answer != hiddenNumber
+    ? (counter--,
+      console.log(
+        `Try again, but ${
+          answer.answer > hiddenNumber ? 'lower' : 'higher'
+        }!! ${counter} more opportunities. `
+      ),
+      inquirer.prompt(question).then(recursive))
+    : console.log('YOU WON!!!');
+}
