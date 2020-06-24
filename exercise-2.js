60bf261a5d60b6a117cb854c0be55c4ba2dde3d6
Aquarius-beans <61715112+Aquarius-beans@users.noreply.github.com>
2020-06-24 16:54:18 -0400

    commit - Manual file upload

---
+++ exercise-2.js
@@ -0,0 +1,36 @@
+// Exercise 2 - `getAddressPosition`
+// ---------------------------------
+
+const rp = require("request-promise");
+const env = require("dotenv");
+env.config();
+const opencage = require("opencage-api-client");
+
+function getAddressPosition(address) {
+    const requestObj = {
+        key: "b1de27a51e34472db2a062fce31d9a00",
+        q: address,
+    };
+
+    return opencage
+        .geocode(requestObj)
+        .then((data) => {
+            if (data.status.code == 200) {
+                if (data.results.length > 0) {
+                    const place = data.results[0];
+                    return place;
+                }
+            } else {
+                console.log("error", data.status.message);
+            }
+        })
+        .then((data) => {
+            //console.log(data.geometry);
+            return data.geometry;
+        })
+        .catch((error) => console.log("error", error.message));
+}
+
+//getAddressPosition("boston");
+
+module.exports = { getAddressPosition };
