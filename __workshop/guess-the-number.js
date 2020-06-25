26c59e05dccbeaefbbb9149f4ba51891fd11a859
n4dz <nadzamini@gmail.com>
2020-06-25 02:13:50 -0400

    guess the number done

--- __workshop/guess-the-number.js
+++ __workshop/guess-the-number.js
@@ -11,3 +11,45 @@
 // Otherwise, tell them whether their guess is lower or higher than the hidden number
 // Loop back
 // The user has lost the game. END
+
+const inquirer = require("inquirer");
+
+const randomNumber = Math.round(Math.random() * 100);
+// console.log(randomNumber); // ;)
+let guessNumber = 5;
+
+function guessTheNumber() {
+  guessNumber = guessNumber - 1;
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "question",
+        message: "Try to find the lucky number form 0 to 100!",
+      },
+    ])
+    .then((answers) => {
+      if (guessNumber == 0) {
+        console.log("You lost!");
+      } else if (answers.question == randomNumber) {
+        console.log("You won!");
+      } else if (answers.question > randomNumber) {
+        console.log(
+          "The number you entered is too high. " +
+            guessNumber +
+            " guesses left."
+        );
+        guessTheNumber();
+      } else if (answers.question < randomNumber) {
+        console.log(
+          "The number you entered is too low. " + guessNumber + " guesses left."
+        );
+        guessTheNumber();
+      } else {
+        console.log("Oops, the number must be between 0 and 100!");
+      }
+    });
+}
+
+guessTheNumber();
