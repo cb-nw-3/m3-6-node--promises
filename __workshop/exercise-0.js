cd18deb6582cffeae0b400d34bd596e1db936846
Przemek Piwek <przemekpiwek@gmail.com>
2020-06-26 12:20:52 -0600

    Exercise 1 complete

--- __workshop/exercise-0.js
+++ __workshop/exercise-0.js
@@ -4,26 +4,25 @@
 // a Promise that tests if the value is less than or greater than the value 10.
 
 const compareToTen = (num) => {
-    myPromise = new Promise((resolve, reject) => {
-        if (num > 10) {
-            resolve(num + " is greater than 10, success!")
-        } else {
-            reject(num + " is less than 10, error!")
-        }
-    })
-    return myPromise;
-}
+  myPromise = new Promise((resolve, reject) => {
+    if (num > 10) {
+      resolve(num + " is greater than 10, success!");
+    } else {
+      reject(num + " is less than 10, error!");
+    }
+  });
+  return myPromise;
+};
 
 // Calling the Promise
 compareToTen(15)
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
 
 compareToTen(8)
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
 
-    
 // Exercise 0.1
 // ------------
 // Write two functions that use Promises that you can chain!
@@ -31,29 +30,37 @@ compareToTen(8)
 // and then the second function, sortWords(), will sort the words in alphabetical order.
 // If the array contains anything but strings, it should throw an error.
 
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado']
-const complicatedArray = ['cucumber', 44, true]
-  
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
+
 const makeAllCaps = (array) => {
-    
-}
+  promise = new Promise((resolve, reject) => {
+    if (array.every((item) => typeof item === "string")) {
+      resolve(array.map((item) => item.toUpperCase()));
+    } else {
+      reject("item is not a string");
+    }
+  });
+  return promise;
+};
 
 const sortWords = (array) => {
-    
-} 
+  return new Promise((resolve, reject) => {
+    if (array.every((word) => typeof word === "string")) {
+      resolve(array.sort());
+    } else {
+      reject("items are not strings");
+    }
+  });
+};
 
 // Calling (testing)
 makeAllCaps(arrayOfWords)
-.then(sortWords)
-.then((result) => console.log(result))
-.catch(error => console.log(error))
+  .then(sortWords)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
 
-makeAllCaps(complicatedArray)
-.then(sortWords)
-.then((result) => console.log(result))
-.catch(error => console.log(error))
-
-    
-    
-    
-    
+// makeAllCaps(complicatedArray)
+//   .then(sortWords)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -2,9 +2,21 @@
 // -----------------------------
 
 // require the 'request-promise' module.
-
+const rp = require("request-promise");
 
 // Returns the current position of the ISS
 function getIssPosition() {
-
+  rp("https://api.wheretheiss.at/v1/satellites/25544")
+    .then((result) => {
+      console.log(
+        `The ISS is currently at: Longitude: ${
+          JSON.parse(result).longitude
+        }, Latitude: ${JSON.parse(result).latitude}`
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+
+getIssPosition();
