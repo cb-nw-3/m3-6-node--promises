1b4e114900cdcc57325ed4906e4293556d7e9b5b
David Elliott <dave.elliott@gmail.com>
2020-06-27 12:31:56 -0400

    all exercises done

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -1,10 +1,29 @@
 // Exercise 2 - `getAddressPosition`
 // ---------------------------------
 
-function getAddressPosition(address) {
-    const requestObj = {
-        key: '<MY_API_KEY>',
-        q: address
-    };
+require('dotenv').config();
 
+var rp = require('request-promise');
+const { get } = require('request-promise');
+
+let openCageKey = process.env.OPENCAGE_API_KEY;
+
+const opencage = require('opencage-api-client');
+
+function getOpenCageResult()
+{
+    opencage
+    .geocode({ q: '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8', key: openCageKey })
+    .then(data => { 
+      let first_hit = data.results[0].geometry;
+      console.log({first_hit});
+      // console.log(JSON.stringify(data.results[0].annotations.DMS));
+    })
+    .catch(error => {
+      console.log('error', error.message);
+    });
 }
+
+
+getOpenCageResult();
+
