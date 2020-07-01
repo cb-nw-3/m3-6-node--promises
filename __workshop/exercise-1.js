93d8f57d7d3d303edc7324ba167928ba90649811
carldumas <dumas.carl@gmail.com>
2020-07-01 16:11:04 -0400

    M3-6: Node Promises - Exercise-1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -2,9 +2,24 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const rp = require('request-promise');
 
 
 // Returns the current position of the ISS
 function getIssPosition() {
-
+    return rp('http://api.open-notify.org/iss-snow.json')
+    .then((resultString) => {
+        return JSON.parse(resultString);
+    })
+    .then((resultObject) => {
+        return {
+            lat: resultObject.iss_position.latitude,
+            lng: resultObject.iss_position.longitude,
+        };
+    })
+    .catch(function (err) {
+        return ('Error: ',  err)
+    });
 }
+
+getIssPosition().then((res) => console.log(res));
