d164eb31b28d33fe41d498dac3d737839ae89de7
Alex Langevin <alex_langevin@hotmail.ca>
2020-07-03 15:01:42 -0400

    Ex. 2 Completed

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -1,10 +1,34 @@
 // Exercise 2 - `getAddressPosition`
 // ---------------------------------
 
+const opencage = require('opencage-api-client');
+require('dotenv').config();
+
 function getAddressPosition(address) {
     const requestObj = {
-        key: '<MY_API_KEY>',
+        key: process.env.OPENCAGE_API_KEY,
         q: address
     };
 
-}
+    return opencage.geocode(requestObj)
+        .then( data => {
+            if(data.status.code == 200){
+                if (data.results.length > 0) {
+                    const place = data.results[0];
+                    return place;
+                }
+            } else if (data.status.code == 402){
+                console.log('hit free-trial daily limit');
+            } else {
+                console.log('error', data.status.message);
+            }})
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+        .catch(error => {
+            console.log('error', error.message);
+        });
+    }
+
+getAddressPosition('1150 Van Horne, Montréal, QC H2V 1K2');
