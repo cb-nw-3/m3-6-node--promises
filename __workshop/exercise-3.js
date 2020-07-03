f96e6453a8fe074ba1ee1b43ed2ea83c7c078496
anwesh-sarkar <anwesh.sarkar@gmail.com>
2020-07-03 00:19:05 -0400

    Exercise 3 finished

--- __workshop/exercise-3.js
+++ __workshop/exercise-3.js
@@ -1,7 +1,25 @@
 // Exercise 3 - `getAddressPosition`
 // ---------------------------------
+const dotenv = require("dotenv").config();
+const rp = require("request-promise");
 
 // Given a position (latitude and longitude), returns the position
 function getCurrentTemperatureAtPosition(position) {
-
+  rp(
+    `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${position.latitude},${position.longitude}?units=si`
+  )
+    .then((result) => {
+      return JSON.parse(result);
+    })
+    .then((data) => {
+      console.log("Temperature: ", data.currently.temperature);
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
 }
+
+getCurrentTemperatureAtPosition({
+  latitude: 48.1341651,
+  longitude: 11.5464794,
+});
